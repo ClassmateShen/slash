ca378ec4eb536f231f6f8108ce1a5834926d86ff
叫我沈同学 <97521809+ClassmateShen@users.noreply.github.com>
2025-09-29 11:20:36 +0800

    Update

--- frontend/web/src/stores/subscription.ts
+++ frontend/web/src/stores/subscription.ts
@@ -2,9 +2,9 @@ import { PlanType } from "@/types/proto/api/v1/subscription_service";
 
 export const stringifyPlanType = (planType: PlanType = PlanType.FREE) => {
   if (planType === PlanType.FREE) {
-    return "Free";
+    return "Team";
   } else if (planType === PlanType.PRO) {
-    return "Pro";
+    return "Team";
   } else if (planType === PlanType.ENTERPRISE) {
     return "Team";
   } else {
